4111879c5c13dd99bbf5a832cb2467d0d598e3dc
DALLIN11soccer <noodletrooper6@gmail.com>
2024-11-21 19:36:14 -0700

    Got Database Working

--- service/database.js
+++ service/database.js
@@ -1,41 +1,64 @@
 const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 const config = require('./dbConfig.json');
 
-async function main() {
-  // Connect to the database cluster
-  const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
-  const client = new MongoClient(url);
-  const db = client.db('rental');
-  const collection = db.collection('house');
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('pivot');
+const userCollection = db.collection('users');
+const eventCollection = db.collection('events');
 
-  // Test that you can connect to the database
-  (async function testConnection() {
-    await client.connect();
-    await db.command({ ping: 1 });
-  })().catch((ex) => {
-    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
-    process.exit(1);
-  });
+(async function testConnection() {
+  await client.connect();
+  await db.command({ ping: 1 });
+})().catch((ex) => {
+  console.error('Connection failed', ex);
+  process.exit(1);
+});
 
-  // Insert a document
-  const house = {
-    name: 'Beachfront views',
-    summary: 'From your bedroom to the beach, no shoes required',
-    property_type: 'Condo',
-    beds: 1,
-  };
-  await collection.insertOne(house);
-
-  // Query the documents
-  const query = { property_type: 'Condo', beds: { $lt: 2 } };
-  const options = {
-    sort: { score: -1 },
-    limit: 10,
-  };
-
-  const cursor = collection.find(query, options);
-  const rentals = await cursor.toArray();
-  rentals.forEach((i) => console.log(i));
+/*--------------------------------------------------------------*/
+/*                       Endpoints                              */
+/*--------------------------------------------------------------*/
+function getUser(username) {
+  return userCollection.findOne({ username: username });
 }
 
-main().catch(console.error);
+async function getEvents(userID) {
+  const cursor = eventCollection.find({ userID: userID });
+  return cursor.toArray();
+}
+
+async function createUser(username, password) {
+  // Hash the password before we insert it into the database
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    username: username,
+    password: passwordHash,
+    userID: uuid.v4(),
+  };
+  await userCollection.insertOne(user);
+
+  return user;
+}
+
+async function createEvent(event) {
+  const newEvent = {
+    ...event,
+    eventID: uuid.v4(),
+  };
+  await eventCollection.insertOne(newEvent);
+
+  return newEvent;
+}
+
+/*--------------------------------------------------------------*/
+/*                       Export                                 */
+/*--------------------------------------------------------------*/
+module.exports = {
+  getUser,
+  createUser,
+  getEvents,
+  createEvent
+};
