8c5846a73263156ebcd80cd72df4034d797defa3
DALLIN11soccer <noodletrooper6@gmail.com>
2024-12-07 15:05:59 -0700

    started work on Add Task

--- src/components/forms/AddTask.jsx
+++ src/components/forms/AddTask.jsx
@@ -1,33 +1,28 @@
-import Modal from './../modal';
-import { ModalHeaderDefault, ModalFooterDefault } from './../modal';
-
-export function AddTaskModalFormButton() {
-    return (
-        <button
-            type="button"
-            className="btn btn-primary"
-            data-toggle="modal"
-            data-target="#addTaskModal"
-        >
-            Add Task
-        </button>
-    );
-}
-
-export function AddTaskModalForm(){
-    return (
-        <>
-        <Modal
-            id="#addTaskModal"
-            header={<ModalHeaderDefault title="Task" />}
-            footer={<ModalFooterDefault button1="Cancel" button2="Add Task"/>}
-        >
-            <form>
-                stuff
+export function AddTaskModalForm({ task }) {
+  return (
+    <div className="modal fade" id="addTask" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">Add Task</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div className="modal-body">
+            <form> 
+              <div className="mb-3">
+                <label>Stuff</label>
+                <input type="text" className="form-control" id="stuff" placeholder="Stuff" />
+              </div>
             </form>
-        </Modal>
-        </>
-    );
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" className="btn btn-primary btn-primary-2">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AddTaskModalForm;
