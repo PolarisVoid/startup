d43d452e1b857101f149253d1d1abec99aa49743
DALLIN11soccer <noodletrooper6@gmail.com>
2024-12-07 15:06:55 -0700

    added selected task and project

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -4,10 +4,13 @@ import ProjectHeader from "./ProjectHeader";
 import ProjectBody from "./ProjectBody";
 import { useEffect, useState } from "react";
 import { getEvents, organizeByProject } from "../../javascript/getEvents";
+import AddTaskModalForm from "../../components/forms/AddTask";
 
 function Projects() {
   const [projects, setProjects] = useState({});
   const [selectedProject, setSelectedProject] = useState(null);
+  const [selectedTask, setSelectedTask] = useState(null);
+
   useEffect(() => {
     async function fetchProjects() {
       const events = await getEvents();
@@ -19,7 +22,9 @@ function Projects() {
     }
     fetchProjects();
   }, []);
+
   let projectHeaders = projects ? Object.keys(projects) : [];
+
   return (
     <div className="project-container">
       <ProjectSideMenu
@@ -31,11 +36,14 @@ function Projects() {
       />
       <div className="project-window">
         <ProjectHeader selectedProject={selectedProject}
-        changeSelectedProject={(project) => {
-          setSelectedProject(project);
-        }}
-        projectHeaders={projectHeaders} />
+          changeSelectedProject={(project) => {
+            setSelectedProject(project);
+          }}
+          projectHeaders={projectHeaders} />
         <ProjectBody project={projects[selectedProject]} />
+      </div>
+      <div>
+        <AddTaskModalForm task={selectedTask}/>
       </div>
     </div>
   );
